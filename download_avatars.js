05042903d9beef462f4b0c7a442a16bbf9d02578
Angela Larryant <angelarryant@gmail.com>
2018-10-01 23:25:42 +0000

    Add authorization header

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,25 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+function testingError(err,result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+}
+
+getRepoContributors("jquery", "jquery", testingError);
