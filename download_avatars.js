343d79a41299145779b8d53aa377a5a1c6a38f47
Angela Larryant <angelarryant@gmail.com>
2018-10-02 00:19:01 +0000

    Created array of avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,25 +1,35 @@
 var request = require('request');
 var secrets = require('./secrets.js');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+// console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrets.GITHUB_TOKEN
+      'Authorization': 'token' + secrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    if (err) {
+      cb(err);
+    } else {
+      var formattedData = JSON.parse(body);
+      cb(null, getAvatarUrls(formattedData));
+    }
   });
 }
 
-function testingError(err,result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+
+function getAvatarUrls(formattedData) {
+  return formattedData.map(function(element) {
+    return element.avatar_url;
+  });
 }
 
-getRepoContributors("jquery", "jquery", testingError);
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Results:", result);
+});
